5209d17330c6fd1fc717a3bbd64181d2fa435372
Goat <gzhseo@163.com>
2016-08-19 15:21:54 +0800

    Create test_printArrayBySlash.js

---
+++ test/test_printArrayBySlash.js
@@ -0,0 +1,11 @@
+var printArrayBySlash = require('../printArrayBySlash');
+
+//二维数组的行、列应该是随机比较好，后面在改喽
+var array = [
+[1,2,3,4],
+[5,6,7,8],
+[9,10,11,12],
+[13,14,15,16]
+];
+
+printArrayBySlash(array);
