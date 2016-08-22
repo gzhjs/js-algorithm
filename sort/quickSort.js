d6692e7cc46bbf56aaaa4cd524fa33471aa5f1b7
Goat <gzhseo@163.com>
2016-08-22 20:58:39 +0800

    Update quickSort.js

--- sort/quickSort.js
+++ sort/quickSort.js
@@ -1,32 +1,55 @@
 //快速排序
 //时间复杂度O(nlogn)
-    function quickSort(arr) {
-        var len = arr.length;
 
-        if (len === 0 || len === 1) return arr;
+//第一种方法 直接使用数组方法，代码量少
+function quickSort(arr) {
+    if (!Array.isArray(arr)) throw new TypeError('不是数组');
+    if (arr.length <= 1) return arr;
 
-        var i = 0,
-            mid = Math.round(len / 2),
-            j = mid;
-        while (i < mid || j < len) {
-            if (i < mid) {
-                if (arr[i] > arr[mid]) {
-                    arr.splice(arr[mid + 1], 0, arr.splice(i, 1)[0]);
-                } else {
-                    ++i;
-                }
-            }
+    var key = arr[0];
+    var left = [],
+        right = [];
 
-            if (j < len) {
-                if (arr[j] < arr[mid]) {
-                    arr.splice(arr[mid - 1], 0, arr.splice(j, 1)[0]);
-                } else {
-                    ++j;
-                }
-            }
+    for (var i = 1, len = arr.length; i < len; ++i) {
+        arr[i] > key ? right.push(arr[i]) : left.push(arr[i]);
+    }
+
+    return quickSort(left).concat([key], quickSort(right));
+}
+    
+//第二种方法 尽量避免使用数组方法，代码量大
+function quickSort(arr) {
+    var len = arr.length;
+
+    if (len <= 1) return arr;
+
+    var i = 0,
+        mid = Math.round(len / 2),
+        j = len - 1;
+    var left = [],
+        right = [];
+
+    while (i < mid || j > mid) {
+
+        while (arr[i] <= arr[mid] && i < mid) {
+            left.push(arr[i]);
+            i++;
+        }
+        if (i < mid) {
+            right.push(arr[i]);
+            i++;
         }
 
-        var temp = quickSort(arr.slice(0, mid));
-        temp.push(arr[mid]);
-        return temp.concat(quickSort(arr.slice(mid + 1)));
+        while (arr[j] >= arr[mid] && j > mid) {
+            right.push(arr[j]);
+            j--;
+        }
+        if (j > mid) {
+            left.push(arr[j]);
+            j--;
+        }
     }
+
+
+    return quickSort(left).concat([arr[mid]], quickSort(right));
+}
